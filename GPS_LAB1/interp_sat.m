e3c8d01a4e4009675b273b1ac574ba35f21e853a
David Cristóbal <david@juanked.es>
2019-09-25 23:11:28 +0200

    añadido cdT

--- GPS_LAB1/interp_sat.m
+++ GPS_LAB1/interp_sat.m
@@ -11,27 +11,29 @@ function [XYZ, cdT]=interp_sat(sp,tow,PRN,N)
 
 % Siguiendo lo realizado en el ejercicio 2:
 
-h=900;
+
 sp2=read_sp3(sp);
+h=sp2.delta;
+sp2.tow(1);
 % primero=sp2.tow(1);
 % seg=9*3600+20*60;
 t=tow;
-x=(t-sp2.tow(1))/sp2.delta;
+x=(t-sp2.tow(1))/h;
 prev=floor(x)+1;
 rg=(prev-N/2+1:prev+N/2);
-
 dif_XYZ = zeros(3,N);
 
     for j=1:3
-        ck=sp2.XYZ(j,rg,25); %Para obtener cada coordenada
+        ck=sp2.XYZ(j,rg,PRN); %Para obtener cada coordenada
         dif_XYZ(j,:)=get_df(ck);
     end
 
-
-s=(N/2-1)+mod(tow,h)/h;
+s=(N/2-1)+mod(t,h)/h;
 S=get_S(s,N);
 XYZ=dif_XYZ*S;
-fprintf('XYZ= [%.2f %.2f %.2f]',XYZ)
+cdt=sp2.cdT(PRN,rg);
+cdT=cdt*S;
+%fprintf('XYZ= [%.2f %.2f %.2f]',XYZ);
 
 return
 
@@ -56,10 +58,8 @@ function S = get_S(s,N)
 % Recibe s y devuelve vector de coefs S para usar en interpolación
     dS=zeros(N,1);
     dS(1,1)=1;
-    for i=2:N
-        k=i-1;
-        dS(i,1) = dS(i-1,1)*((s-k+1)/k);
+    for i=1:N-1
+        dS(i+1,1) = dS(i,1)*((s-i+1)/i);
     end
 S=dS;
 return
-
