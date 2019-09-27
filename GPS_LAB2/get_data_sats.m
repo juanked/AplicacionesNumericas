fd9955be581726eec04ad8682e27e68d83d9e270
David Cristóbal <david@juanked.es>
2019-09-27 11:00:24 +0200

    get_data_stats arreglado

--- GPS_LAB2/get_data_sats.m
+++ GPS_LAB2/get_data_sats.m
@@ -4,7 +4,7 @@ if nargin==3, N=8; end  % Si no se especifica, usamos N = 8 nodos
  % Si t = �nico, crea vector de t's iguales 
  if length(t)==1, t=t*ones(1,nsat); end  
  for i=1:nsat
-     [XYZ, cdT]=interp_sat(sp,t,prn(i),N);
+     [XYZ, cdT]=interp_sat(sp,t(i),prn(i),N);
      XYZ_sat(:,i)=XYZ;
      cdT(i,1)=cdT;
  end
