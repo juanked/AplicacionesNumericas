11b9095fd00c6c00fff2fa33d5bc4ab267130ed6
David Cristóbal <david@juanked.es>
2019-09-30 21:07:45 +0200

    Solucionado problema cdT

--- GPS_LAB2/get_data_sats.m
+++ GPS_LAB2/get_data_sats.m
@@ -4,9 +4,10 @@ if nargin==3, N=8; end  % Si no se especifica, usamos N = 8 nodos
  % Si t = �nico, crea vector de t's iguales 
  if length(t)==1, t=t*ones(1,nsat); end  
  for i=1:nsat
-     [XYZ, cdT]=interp_sat(sp,t(i),prn(i),N);
+     [XYZ, cdTa]=interp_sat(sp,t(i),prn(i),N);
      XYZ_sat(:,i)=XYZ;
-     cdT(i,1)=cdT;
+     cdT(i,1)=cdTa;
  end
  %find(PRN == sp.prn)
+ 
 return
