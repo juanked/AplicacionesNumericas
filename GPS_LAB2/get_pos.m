a11724a8e673143805bda5380c6b104ffa183848
David Cristóbal <david@juanked.es>
2019-10-09 23:52:05 +0200

    Entrega final

--- GPS_LAB2/get_pos.m
+++ GPS_LAB2/get_pos.m
@@ -1,3 +1,7 @@
+% David Cristóbal Pascual
+% Alberto Doncel Aparicio
+
+
 function X = get_pos(sp, Tr, sats, obs, X, N)
 
     if nargin < 6, N = 8; end% Si no se especifica, usamos N = 8 nodos
@@ -12,18 +16,7 @@ function X = get_pos(sp, Tr, sats, obs, X, N)
                 sats(i) = find(sats(i) == sp.prn);
                 i = i + 1;
             end
-
-            %     if (length(Tr)==1)
-            %         Tr2=zeros(1,length(sats));
-            %         i=1;
-            %         while (i<length(sats)+1)
-            %             Tr2(i)=Tr;
-            %
-            %             i=i+1;
-            %         end
-            %     end
-            i = 1;
-
+            
             while (1)
                 
                 pos = X(1:3, 1);
@@ -41,5 +34,32 @@ function X = get_pos(sp, Tr, sats, obs, X, N)
                 end
 
             end
+return
 
-            return
+function [XYZ_sat, cdT]=get_data_sats(sp,t,prn,N)
+if nargin==3, N=8; end  % Si no se especifica, usamos N = 8 nodos
+ nsat = length(prn); XYZ_sat=zeros(3,nsat); cdT=zeros(nsat,1);  
+ % Si t = �nico, crea vector de t's iguales 
+ if length(t)==1, t=t*ones(1,nsat); end  
+ for i=1:nsat
+     [XYZ, cdTa]=interp_sat(sp,t(i),prn(i),N);
+     XYZ_sat(:,i)=XYZ;
+     cdT(i,1)=cdTa;
+ end
+ %find(PRN == sp.prn)
+ 
+return
+
+function [H,R]=get_HR(XYZ,pos)
+nsat = size(XYZ,2);
+H=zeros(nsat,4);
+R=zeros(nsat,1);
+for i=1:nsat
+   R(i,1)=sqrt(((XYZ(1,i)-pos(1))^2)+((XYZ(2,i)-pos(2))^2)+((XYZ(3,i)-pos(3))^2));
+   H(i,1)=(-(XYZ(1,i)-pos(1)))/(R(i,1));
+   H(i,2)=(-(XYZ(2,i)-pos(2)))/(R(i,1));
+   H(i,3)=(-(XYZ(3,i)-pos(3)))/(R(i,1));%H(i,3)=(-(XYZ(3,1)-pos(3)))/(R(i,1));
+   H(i,4)=1;
+end
+
+return
